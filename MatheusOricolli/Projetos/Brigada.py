58ff838f115d03db3ce1ba6d9cee6d6a5b2fc814
Matheus Oricolli <oricollidasilvamatheus@gmail.com>
2026-05-21 07:41:23 -0300

    Atualização da Brigada

--- MatheusOricolli/Projetos/Brigada.py
+++ MatheusOricolli/Projetos/Brigada.py
@@ -1,14 +1,8 @@
-print("Olá! Seja bem vindo ao sistema de controle da SESMT (Serviço Especializado em Engenharia de Segurança e em Medicina do Trabalho) ")
+print("Olá! Seja bem vindo ao sistema de controle da SESMT (Serviço Especializado em Engenharia de Segurança e em Medicina do Trabalho)")
 print("Informe seus dados abaixo: \n")
 
 total_cadastrados = 0
 total_em_dia = 0
-
-nome = input("Digite o nome do funcionário: ")
-setor = input("Digite o setor (Elétrica / Trabalho em Altura): ")
-nr10 = input("Status NR-10 (OK/Pendente): ")
-nr35 = input("Status NR-35 (OK/Pendente): ")
-ano_brigada = int(input("Ano do último treinamento da Brigada: "))
 
 def verificar_epi(setor_escolhido):
     print(f"Setor: {setor_escolhido}")
@@ -19,7 +13,6 @@ def verificar_epi(setor_escolhido):
     else:
         print("EPIs: Consultar setor de segurança.")
 
-# Função para o Requisito 3: Alerta de Reciclagem
 def alerta_reciclagem(ano_treino):
     ano_atual = 2026
     if (ano_atual - ano_treino) > 2:
@@ -29,17 +22,29 @@ def alerta_reciclagem(ano_treino):
         print("MENSAGEM: Treinamento Válido.")
         return "OK"
 
-total_cadastrados = total_cadastrados + 1
+while True:
+    nome = input("\nDigite o nome do funcionário: ")
+    setor = input("Digite o setor (Elétrica / Trabalho em Altura): ")
+    nr10 = input("Status NR-10 (OK/Pendente): ")
+    nr35 = input("Status NR-35 (OK/Pendente): ")
+    ano_brigada = int(input("Ano do último treinamento da Brigada: "))
 
-print("\n--- RESULTADOS ---")
-verificar_epi(setor)
-status_brigada = alerta_reciclagem(ano_brigada)
+    total_cadastrados = total_cadastrados + 1
 
-if nr10 == "OK" and nr35 == "OK" and status_brigada == "OK":
-    total_em_dia = total_em_dia + 1
+    print("\n--- RESULTADOS ---")
+    verificar_epi(setor)
+    status_brigada = alerta_reciclagem(ano_brigada)
 
-print("\n" + "="*30)
-print("RELATÓRIO GERAL")
-print(f"Total de funcionários cadastrados: {total_cadastrados}")
-print(f"Funcionários com treinamentos em dia: {total_em_dia}")
-print("="*30)
+    if nr10 == "OK" and nr35 == "OK" and status_brigada == "OK":
+        total_em_dia = total_em_dia + 1
+    continuar = input("\nDeseja realizar mais um cadastro? (Sim/Não): ").strip().lower()
+    
+    if continuar not in ["sim", "s"]:
+        break
+
+print("\n" + "="*60)
+print(f"{'RELATÓRIO GERAL':^60}")
+print(f"{'-'*25:^60}")
+print(f"Total de funcionários cadastrados: {total_cadastrados:>6}")
+print(f"Funcionários com treinamentos em dia: {total_em_dia:>3}")
+print("="*60)
